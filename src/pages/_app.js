d9d5733880be98746c67d2dba74b6e3d521161fe
Kristjan Poska <kristjan.poska@guardtime.com>
2023-02-23 09:34:35 +0200

    Change some styling & add initial content

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Kristjan Poska</title>
+        <link rel="shortcut icon" href="favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div>Hello world</div>;
+import Link from 'next/link';
+import styles from '@/styles/Index.module.scss';
+
+export default function Index() {
+  return (
+    <>
+      <h1 className={styles.heading}>Kristjan Poska</h1>
+
+      <section className={styles.section}>
+        <p>
+          Striving for <em>perfection</em> in everything I do. Experimenting
+          with all the unseen details in user interfaces. Front-end developer at{' '}
+          <Link href="https://guardtime.com/">Guardtime</Link>. Interaction
+          Design student at <Link href="https://ixd.ma/">ixd.ma</Link>.
+        </p>
+
+        <p>
+          In the past, I&apos;ve blown steam off at{' '}
+          <Link href="https://voog.com/">Voog</Link> to build the best CMS in
+          the Baltics.
+        </p>
+      </section>
+    </>
+  );
 }
