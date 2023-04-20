6d47a4da87800d351c82403d50992dc6a9a85ad1
Kristjan Poska <kristjan.poska@guardtime.com>
2023-04-20 08:20:53 +0300

    Fix the Tangible project page

--- src/components/Craft/tangible.js
+++ src/components/Craft/tangible.js
@@ -7,9 +7,9 @@ import thirdImage from '../../assets/images/IMG_2693.jpg';
 
 const Tangible = () => {
   return (
-    <div className="flex">
+    <div className="flex gap-8">
       <div className="w-1/2">
-        <p className="pb-4 max-w-prose">
+        <p className="pb-4">
           Introducing Playser, the revolutionary musical device that is changing
           the way people explore their creativity through music! Playser is
           designed to meet the needs of modern-day living, offering a compact
@@ -19,7 +19,7 @@ const Tangible = () => {
           MIDI outputs with a simple touch of its eight built-in lasers.
         </p>
 
-        <p className="pb-4 max-w-prose">
+        <p className="pb-4">
           The concept behind Playser is that our hands will be the future remote
           controller to play music. It flirts with the idea that technologies
           will be embedded within us, whether it be through gestures or
@@ -27,7 +27,7 @@ const Tangible = () => {
           make it easier and easier to use future technologies.
         </p>
 
-        <p className="pb-4 max-w-prose">
+        <p className="pb-4">
           During the design process, the team struggled to brainstorm an idea
           that would fit the theme of &quot;Smart Art.&quot; After exploring
           various ideas, they came up with the initial concept of Playser, which
@@ -37,7 +37,7 @@ const Tangible = () => {
           visual impairments.
         </p>
 
-        <p className="pb-4 max-w-prose">
+        <p className="pb-4">
           The building process was divided into different tasks, such as
           electronics, laser cutting, and designing. Through this process, the
           team learned to design prototypes in Adobe Illustrator, laser cut them
@@ -45,7 +45,7 @@ const Tangible = () => {
           consider the final appearance of the product.
         </p>
 
-        <p className="pb-4 max-w-prose">
+        <p className="pb-4">
           Lastly, while AI provided valuable creative insights during the design
           process, the team discovered that when given prompts that required
           truly imaginative ideas, AI falls somewhat short. Although AI did
@@ -53,7 +53,7 @@ const Tangible = () => {
           team&apos;s concept through the prompts provided.
         </p>
 
-        <p className="pb-4 max-w-prose">
+        <p className="pb-4">
           In conclusion, Playser is a device that offers a new way to explore
           your musical creativity while meeting the needs of modern-day living.
           It is designed to be visually appealing, user-friendly, and accessible
@@ -63,10 +63,22 @@ const Tangible = () => {
           prototype.
         </p>
       </div>
-      <div className="grid grid-cols-2 gap-4">
-        <Image src={firstImage} alt="text" className="grid-gap-4" />
-        <Image src={secondImage} className="grid-gap-4" alt="text" />
-        <Image src={thirdImage} className="grid-gap-4" alt="text" />
+      <div className="w-1/2 flex flex-col gap-4">
+        <Image
+          src={firstImage}
+          className="float-left"
+          alt="Lasers pointing at a piece of paper do designate the notes"
+        />
+        <Image
+          src={secondImage}
+          className="float-left"
+          alt="Sketching out how to place the lasers"
+        />
+        <Image
+          src={thirdImage}
+          className="float-left"
+          alt="Coloring the prototype orange"
+        />
       </div>
     </div>
   );
