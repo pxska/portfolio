d94b1676e063da7ae8ffed4447020ef6c647ec15
Kristjan Poska <kristjan.poska@guardtime.com>
2023-04-29 21:36:14 +0300

    Will it refresh now?

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,13 +3,12 @@ import styles from './Card.module.scss';
 const COLORS = ['#DCB481', '#C09E85', '#E0D0C3', '#90837A', '#B9B99D'];
 
 const Card = ({href, icon, title, description}) => {
+  const backgroundColorStyles = {
+    backgroundColor: COLORS[Math.floor(Math.random() * COLORS.length)],
+  };
+
   return (
-    <a
-      href={href}
-      className={styles.card}
-      style={{
-        backgroundColor: COLORS[Math.floor(Math.random() * COLORS.length)],
-      }}>
+    <a href={href} className={styles.card} style={backgroundColorStyles}>
       <div className={styles.icon}>{icon}</div>
       <div className={styles.description}>
         <span className={styles.title}>{title}&nbsp;</span>
